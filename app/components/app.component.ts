e06c6c39b65c3be005918886a2bcf847eeb44630
fcamblor <fcamblor@gmail.com>
2021-03-28 22:58:02 +0200

    introduced data sharing between counters, by registering a single data state at application level, and using event to bubble counter changes

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,13 +1,15 @@
-import {customElement, html, LitElement} from "lit-element";
+import {customElement, html, LitElement, property} from "lit-element";
 
 @customElement('hello-app')
 class AppComponent extends LitElement {
+    @property({type: Number}) value = 2;
+
     render() {
         return html`
-          <hello-counter value="10" max="20">
+          <hello-counter .value="${this.value}" @value-changed="${(e: HTMLElementEventMap['value-changed']) => this.value = e.detail.value }" max="20">
             <p slot="content">Hello <strong>world</strong> !</p>
           </hello-counter>
-          <hello-counter value="10" max="20"></hello-counter>
+          <hello-counter .value="${this.value}" @value-changed="${(e: HTMLElementEventMap['value-changed']) => this.value = e.detail.value }" max="10"></hello-counter>
         `;
     }
 }
--- app/components/counter.component.ts
+++ app/components/counter.component.ts
@@ -2,27 +2,41 @@ import { LitElement, html, customElement, property } from 'lit-element'
 
 @customElement('hello-counter')
 export class CounterComponent extends LitElement {
-    @property({ type: Number }) value = 1;
+    @property({ type: Number }) set value(val: number) {
+        const newVal = Math.min(val, this.max);
+        if(this._value !== newVal) {
+            this._value = newVal;
+            this.requestUpdate('value');
+            this.dispatchEvent(new CustomEvent('value-changed', {
+                detail: {
+                    value: newVal
+                }
+            }));
+        }
+    };
+    get value(){ return this._value; }
+    private _value = 1;
+
     @property({type: Number}) max = Infinity;
 
     render() {
         return html`
             <div>
               Counter : 
-              <button @click="${this.increment}">+</button>
+              <button @click="${() => this.value++}">+</button>
               ${this.value}
-              <button @click="${this.decrement}">-</button>
+              <button @click="${() => this.value--}">-</button>
               <slot name="content"></slot>
             </div>
         `;
     }
-
-    increment() { this.value = Math.min(this.value+1, this.max); }
-    decrement() { this.value--; }
 }
 
 declare global {
     interface HTMLElementTagNameMap {
         'hello-counter': CounterComponent
+    }
+    interface HTMLElementEventMap {
+        'value-changed': CustomEvent<{ value: number }>
     }
 }
