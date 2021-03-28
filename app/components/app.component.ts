d6fd83cf32545dc69d1eb02c70a37a1a6b5fc8fe
fcamblor <fcamblor@gmail.com>
2021-03-28 01:58:20 +0100

    introduced hello-app component and extracted hello-counter component into dedicated component file

---
+++ app/components/app.component.ts
@@ -0,0 +1,18 @@
+import {customElement, html, LitElement} from "lit-element";
+
+@customElement('hello-app')
+class AppComponent extends LitElement {
+    render() {
+        return html`
+          <hello-counter value="10" max="20">
+          </hello-counter>
+          <hello-counter value="10" max="20"></hello-counter>
+        `;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'hello-app': AppComponent
+    }
+}
--- app/index.ts
+++ app/index.ts
@@ -1,27 +1,2 @@
-import { LitElement, html, customElement, property } from 'lit-element'
-
-@customElement('hello-counter')
-class CounterComponent extends LitElement {
-    @property({ type: Number }) value = 1;
-    @property() max = Infinity;
-
-    render() {
-        return html`
-            <div>
-              Counter : 
-              <button @click="${this.increment}">+</button>
-              ${this.value}
-              <button @click="${this.decrement}">-</button>
-            </div>
-        `;
-    }
-
-    increment() { this.value = Math.min(this.value+1, this.max); }
-    decrement() { this.value--; }
-}
-
-declare global {
-    interface HTMLElementTagNameMap {
-        'hello-counter': CounterComponent
-    }
-}
+import './components/app.component'
+import './components/counter.component'
